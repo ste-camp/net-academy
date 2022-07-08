4d8771048fb31c30e0c2857bc0a77dd641a918d8
Stefano Campanella <stefano.campanella@techedgegroup.com>
2022-07-08 15:29:46 +0200

    inizio implementazione controller courses

---
+++ NetAcademy/NetAcademy.Web/Controllers/CoursesController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetAcademy.Domain.Models.DTOs;
+using NetAcademy.Services;
+
+namespace NetAcademy.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CoursesController : ControllerBase
+{
+    private ILogger<CoursesController> logger;
+    private CoursesService service;
+
+    public CoursesController(ILogger<CoursesController> l, CoursesService s)
+    {
+        logger = l;
+        service = s;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<CourseDto>> GetCourse([FromRoute] long id)
+    {
+        var course = await service.GetCourseAsync(id);
+        if (course == null)
+        {
+            return StatusCode(404);
+        }
+        else
+        {
+            return course;
+        }
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> AddCourse([FromBody] CourseDto? data)
+    {
+        if (data != null)
+        {
+            await service.CreateNewCourseAsync(data);
+            return StatusCode(200);
+        }
+        else
+        {
+            return StatusCode(400);
+        }
+    }
+
+    [HttpGet("")]
+    public async Task<List<CourseDto>> GetAllCourses(string category)
+    {
+        return await service.GetAllCoursesAsync(category);
+    }
+}
